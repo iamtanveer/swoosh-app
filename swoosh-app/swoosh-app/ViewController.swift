62b6e616dda344afd1e3164b4c86c2344444737b
Tanveer Singh <tanveer7537@gmail.com>
2017-08-08 19:13:42 +0530

    2 contraints added programatically

--- swoosh-app/swoosh-app/ViewController.swift
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var swoosh: UIImageView!
+    @IBOutlet var bgImg: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        //adding constraints programatically
+        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        //background constraint
+        bgImg.frame = view.frame
+        
     }
 
     override func didReceiveMemoryWarning() {
