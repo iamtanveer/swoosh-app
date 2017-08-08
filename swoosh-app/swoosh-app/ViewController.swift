7578981d93ee42d2359db76f40671f35051e6d22
Tanveer Singh <tanveer7537@gmail.com>
2017-08-08 20:50:17 +0530

    Autolayout used, programatically added constraints removed. The app is now compatible with every iOS Device.

--- swoosh-app/swoosh-app/ViewController.swift
+++ swoosh-app/swoosh-app/ViewController.swift
@@ -15,16 +15,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        //adding constraints programatically
-        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
-        //background constraint
-        bgImg.frame = view.frame
+//        //adding constraints programatically
+//        swoosh.frame = CGRect(x: view.frame.size.width/2 - swoosh.frame.size.width/2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+//
+//        //background constraint
+//        bgImg.frame = view.frame
+        
+        
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
