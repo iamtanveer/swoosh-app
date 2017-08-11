02d2af785d39c9615cd362ee9b63bd7828252afb
Tanveer Singh <tanveer7537@gmail.com>
2017-08-11 22:07:07 +0530

    @IBDesignable added and prepareForInterfaceBuilder() func added

--- swoosh-app/swoosh-app/View/BorderButton.swift
+++ swoosh-app/swoosh-app/View/BorderButton.swift
@@ -7,13 +7,20 @@
 //
 
 import UIKit
+@IBDesignable
 
 class BorderButton: UIButton {
-
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        customizeView()
+    }
+    func customizeView(){
         layer.borderWidth = 3.0
         layer.borderColor = UIColor.white.cgColor
     }
+    
 
 }
